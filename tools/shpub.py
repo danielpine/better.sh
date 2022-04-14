74e60c93aad15ab8ca93b6563afdcf362b340761
danielpine <danielpine@sina.com>
2022-04-14 09:30:27 +0800

    u

--- tools/shpub.py
+++ tools/shpub.py
@@ -79,7 +79,7 @@ def put(url, unique, save):
                             print('error when:'+name)
                         try:
                             PySqlTemplate.save(
-                                'insert into record(name,mark_date,year) values(?,?,?)', name, date, '2022')
+                                'insert into record(name,mark_date,year,district) values(?,?,?)', name, date, '2022', d)
                         except:
                             print('error when:'+name)
                 print('---')
